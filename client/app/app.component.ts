ce0e435bb3696c63558b7c1e6164bf106adb9575
Kevin Amilund <Kevin Amilund>
2017-04-12 21:13:15 +0200

    added some more files and had an error with the app folder, solved it by with code from the tutorial

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,7 +1,11 @@
-    import { Component } from '@angular/core';
-    
-    @Component({
-      selector: 'my-app',
-      template: '<h1>My First Angular App</h1>'
-    })
-    export class AppComponent { }
+import { Component } from '@angular/core';
+import {TaskService} from './services/task.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  providers:[TaskService]
+})
+
+export class AppComponent { }
